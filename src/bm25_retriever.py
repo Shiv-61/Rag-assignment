477c98b142609fcb1415580fe6541533930e8647
Shiv-61 <shivgowda61@gmail.com>
2026-07-24 11:48:55 +0530

    Eval and golden dataset.

---
+++ src/bm25_retriever.py
@@ -0,0 +1,9 @@
+from langchain_community.retrievers import BM25Retriever
+
+def build_bm25_retriever(chunks):
+
+    bm25_retriever = BM25Retriever.from_documents(chunks)
+
+    bm25_retriever.k = 5
+
+    return bm25_retriever
